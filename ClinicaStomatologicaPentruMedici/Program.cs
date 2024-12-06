d9e678e9d707d34dccd222ba6e996cdb88b00d70
STEFÁNIA PATKO <stefania.patko@stud.ubbcluj.ro>
2024-12-06 18:01:40 +0200

    Identity

--- ClinicaStomatologicaPentruMedici/Program.cs
+++ ClinicaStomatologicaPentruMedici/Program.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ClinicaStomatologicaPentruMedici.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Doctors");
+});
 builder.Services.AddDbContext<ClinicaStomatologicaPentruMediciContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ClinicaStomatologicaPentruMediciContext") ?? throw new InvalidOperationException("Connection string 'ClinicaStomatologicaPentruMediciContext' not found.")));
 
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("ClinicaStomatologicaPentruMediciContext") ?? throw new InvalidOperationException("Connectionstring 'ClinicaStomatologicaPentruMediciContext' not found."))); 
+
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
